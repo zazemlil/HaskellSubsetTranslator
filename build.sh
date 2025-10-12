c2e777ee9c0e786c34e96c93348283786b02685d
zazemlil <tiknik19@mail.ru>
2025-10-12 03:38:41 +0300

    build.sh fix (no need include dir)

--- build.sh
+++ build.sh
@@ -2,12 +2,9 @@
 
 SRC_DIR="./src"
 BUILD_DIR="./build"
-INCLUDE_DIR="./include"
 
 rm -rf $BUILD_DIR
-rm -rf $INCLUDE_DIR
 mkdir -p "$BUILD_DIR"/{linux,windows}
-mkdir -p $INCLUDE_DIR
 
 echo "📝 Generating parser..."
 cd $BUILD_DIR
@@ -16,17 +13,15 @@ echo "🔤 Generating lexer..."
 flex ../$SRC_DIR/lexer.l
 cd ..
 
-cp $BUILD_DIR/Parser.hpp $INCLUDE_DIR/
-cp $BUILD_DIR/Scanner.hpp $INCLUDE_DIR/
 
 echo "⚙️ Compiling project..."
-g++ -std=c++17 -I$INCLUDE_DIR -I$SRC_DIR \
+g++ -std=c++17 -I$SRC_DIR \
     $SRC_DIR/main.cpp \
     $BUILD_DIR/Parser.cpp \
     $BUILD_DIR/Scanner.cpp \
     -o $BUILD_DIR/linux/main
 
-x86_64-w64-mingw32-g++ -static -I$INCLUDE_DIR -I$SRC_DIR \
+x86_64-w64-mingw32-g++ -static -I$SRC_DIR \
     $SRC_DIR/main.cpp \
     $BUILD_DIR/Parser.cpp \
     $BUILD_DIR/Scanner.cpp \
