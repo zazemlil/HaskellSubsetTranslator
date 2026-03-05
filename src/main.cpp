033992456f68b0ef1d8040e451be2c7af9e8163d
zazemlil <tiknik19@mail.ru>
2026-03-05 17:02:47 +0300

    main fix (stop process when error)

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,9 @@ int main(int argc, char* argv[])
     }
     catch(const std::exception& e) {
         std::cerr << e.what() << '\n';
+        return -1;
     }
 
+    std::cout << "Success.\n";
     return 0;
 }
