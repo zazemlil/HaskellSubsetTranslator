0cb602eb2710c72dabb6d3f7aa93135ec37abd36
zazemlil <tiknik19@mail.ru>
2026-03-17 01:23:57 +0300

    update CLI

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,70 @@
 #include <iostream>
 #include "AST.h"
 #include "StaticAnalyzer.h"
+#include "Translator.h"
 
-extern std::tuple<syntax_tree::AST, syntax_tree::AST> analyze(int argc, char* argv[]);
+extern std::tuple<syntax_tree::AST, syntax_tree::AST> analyze(char* arg, bool printError);
 
 int main(int argc, char* argv[])
 {
-    auto [ast, dataDeclarations] = analyze(argc, argv);
+    bool s_flag = false, a_flag = false, t_flag = false, ir_flag = false;
+    int new_argc = argc;
+    for (int i = 1; i < new_argc; i++) {
+        if (std::string(argv[i]) == "-s") {
+            s_flag = true;
+            for (int j = i; j < new_argc - 1; j++) {
+                argv[j] = argv[j + 1];
+            }
+            new_argc--;
+            i--;
+        }
+        if (std::string(argv[i]) == "-a") {
+            a_flag = true;
+            for (int j = i; j < new_argc - 1; j++) {
+                argv[j] = argv[j + 1];
+            }
+            new_argc--;
+            i--;
+        }
+        if (std::string(argv[i]) == "+t") {
+            t_flag = true;
+            for (int j = i; j < new_argc - 1; j++) {
+                argv[j] = argv[j + 1];
+            }
+            new_argc--;
+            i--;
+        }
+        if (std::string(argv[i]) == "+ir") {
+            ir_flag = true;
+            for (int j = i; j < new_argc - 1; j++) {
+                argv[j] = argv[j + 1];
+            }
+            new_argc--;
+            i--;
+        }
+    }
+    argc = new_argc;
+
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_file> [<output_file>]" << std::endl;
+        return -1;
+    }
+
+    auto [ast, dataDeclarations] = analyze(argv[1], !s_flag);
     if (ast.isEmpty()) {
         return -1;
     }
-    std::cout << "---------------------- AST ----------------------\n";
-    ast.print();
-    std::cout << "--------------- Data Declarations ---------------\n";
-    dataDeclarations.print();
+
+    if (t_flag) {
+        std::cout << "-------------------------------------------------\n";
+        std::cout << "---------------------- AST ----------------------\n";
+        std::cout << "-------------------------------------------------\n";
+        ast.print();
+        std::cout << "-------------------------------------------------\n";
+        std::cout << "--------------- Data Declarations ---------------\n";
+        std::cout << "-------------------------------------------------\n";
+        dataDeclarations.print();
+    }
 
     StaticAnalyzer* staticAnalyzer = new StaticAnalyzer();
     syntax_tree::AST ir;
@@ -23,13 +74,38 @@ int main(int argc, char* argv[])
         ir = ast;
     }
     catch(const std::exception& e) {
-        std::cerr << e.what() << '\n';
+        if (!a_flag) {
+            std::cerr << e.what() << '\n';
+        } 
         return -2;
     }
 
-    
-    std::cout << "---------------------- IR -----------------------\n";
-    ir.print();
+    if (ir_flag) {
+        std::cout << "-------------------------------------------------\n";
+        std::cout << "---------------------- IR -----------------------\n";
+        std::cout << "-------------------------------------------------\n";
+        ir.print();
+    }
+
+    Translator* translator = new Translator();
+    syntax_tree::AST extendedLambdaCalculus = translator->translate(ir);
+
+    if (argc < 3) {
+        std::cout << "-------------------------------------------------\n";
+        std::cout << "------------ Extended lambda calculus -----------\n";
+        std::cout << "-------------------------------------------------\n";
+        extendedLambdaCalculus.print();
+    } 
+    else {
+        auto fileStream = std::make_unique<std::ofstream>(argv[2], std::ios::out | std::ios::trunc);
+        if (fileStream->is_open()) {
+            extendedLambdaCalculus.print(true, *fileStream);
+            fileStream->close();
+        }
+        else {
+            std::cerr << "The file is not open.\n";
+        }
+    }
 
     std::cout << "Success.\n";
     return 0;
