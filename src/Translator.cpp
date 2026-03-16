263a9a806d057287e6b1fbeebc7a1d659801aaf8
zazemlil <tiknik19@mail.ru>
2026-03-17 00:31:09 +0300

    Translator interface fix

--- src/Translator.cpp
+++ src/Translator.cpp
@@ -1,6 +1,6 @@
 #include "Translator.h"
 
-std::shared_ptr<syntax_tree::ASTNode> Translator::translate(std::shared_ptr<syntax_tree::AST> ir)
+std::shared_ptr<syntax_tree::ASTNode> Translator::translate(syntax_tree::AST ir)
 {
     return std::shared_ptr<syntax_tree::ASTNode>();
 }
--- src/Translator.h
+++ src/Translator.h
@@ -8,7 +8,7 @@ public:
     Translator() = default;
 
     // Основная функция трансляции
-    std::shared_ptr<syntax_tree::ASTNode> translate(std::shared_ptr<syntax_tree::AST> ir);
+    std::shared_ptr<syntax_tree::ASTNode> translate(syntax_tree::AST ir);
 
 private:
 
