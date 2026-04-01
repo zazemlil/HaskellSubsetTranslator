a2b57c4e9a4dcf2e89fe56cd596270838e38a7cc
zazemlil <tiknik19@mail.ru>
2026-04-01 22:48:06 +0300

    to do

--- test.hs
+++ test.hs
@@ -6,3 +6,12 @@ g = (f (\x -> [1, 2]));
 c = (1, 2, 3);
 
 tree = Tree 5 (Leaf 1) (Empty);
+
+-- if -> case (maybe in IRGenerator)
+-- List -> :
+-- List pattern -> :
+-- +Nil
+-- case -> FATBAR
+-- let and where -> application with fix (+check def depend.)
+-- list comprehension
+-- output spaces for FATBAR
